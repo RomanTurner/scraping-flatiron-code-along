85168b569cfa9db95857a03ead1b890acf771d01
Roman Turner <rn.turner@live.com>
2021-04-15 17:20:05 -0700

    done

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,20 @@
+class Course
+    attr_accessor :title, :schedule, :description
 
+    @@all = []
+    def def initialize
+      @@all = self 
+    end
+
+    def self.all
+        @all 
+    end 
+
+    def self.reset_all 
+        @@all.clear 
+    end 
+
+
+
+end
 
